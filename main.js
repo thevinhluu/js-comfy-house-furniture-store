e5725a8584cd0daacd450097b89d1271a22c3118
Luu The Vinh <luuthevinh1988@gmail.com>
2020-06-21 22:37:16 +0700

    getting products

--- main.js
+++ main.js
@@ -13,7 +13,25 @@ const productsDOM = document.querySelector('.products-center');
 let cart = [];
 
 // getting the products
-class Products {}
+class Products {
+	async getProducts() {
+		try {
+			let result = await fetch('products.json');
+			let data = await result.json();
+			let products = data.items;
+			products = products.map(item => {
+				const {title, price} = item.fields;
+				const {id} = item.sys;
+				const image = item.fields.image.fields.file.url;
+				return {title, price, id, image};
+			});
+
+			return products;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+}
 
 // display products
 class UI {}
@@ -24,4 +42,7 @@ class Storage {}
 document.addEventListener('DOMContentLoaded', () => {
 	const ui = new UI();
 	const products = new Products();
+
+	// get all products
+	products.getProducts().then(data => console.log(data));
 });
